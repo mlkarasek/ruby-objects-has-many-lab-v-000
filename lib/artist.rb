5a029cc662c01bc5097d3602d3ece0cf8d59f312
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 03:03:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,5 @@
 class Artist
-
   attr_accessor :name
-
   @@song_count = 0
 
   def initialize(name)
@@ -14,7 +12,7 @@ class Artist
   end
 
   def add_song(song)
-    self.song << collection
+    @songs << song
     song.artist = self
     @@song_count += 1
   end
@@ -26,6 +24,7 @@ class Artist
     @@song_count += 1
   end
 
-  def song_count
+  def self.song_count
     @@song_count
+  end
 end
