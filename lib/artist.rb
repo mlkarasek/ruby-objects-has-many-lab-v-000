59e8a2bc90532cfcbaf6c170a6d583a999fb156c
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 02:59:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,8 +23,8 @@ end
     @@song_count += 1
   end
 
-  def add_song_by_name(song_name)
-    song = self.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
